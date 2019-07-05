afca693c59b6f1f6a0c1beb304ef8b94e1a5d6d4
Weiran Ye <weiran@talkan.com>
2019-07-04 23:25:23 -0700

    bnc-only and frq-only word list

--- import.py
+++ import.py
@@ -1,7 +1,6 @@
 import csv
 import json
 import jsonpickle
-import datetime
 
 class Definition:
   def __init__(self, word, translation, bnc, frq):
@@ -11,17 +10,20 @@ class Definition:
     self.frq = frq
 
 input_filename = 'ecdict.csv'
-output_filename = 'words.json'
-has_either_rank_number = True # the word has some requency ranking
+output_filename = 'words_frq_only.json'
+include_bnc_only = False
+include_frq_only = True
 
 with open(input_filename) as csvfile:
     defs = []
     reader = csv.reader(csvfile)
     print('starting loop')
     for row in reader:
-      if has_either_rank_number and row[8] == '0' and row[9] == '0': 
+      if (include_bnc_only and row[8] == '0'):
         continue
-      # only take: 0 - word; 3 - definition; 8 - bnc; 9 - frq
+      if (include_frq_only and row[9] == '0'): 
+        continue
+      # only take these fields: 0 - word; 3 - definition; 8 - bnc; 9 - frq
       definition = Definition(row[0], row[3], row[8], row[9])
       defs.append(definition)
     print("end loop")
